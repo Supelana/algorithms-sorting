2e89280d3975f3332c3c32c1f5ac444d9d90eec3
Supelana <supelana@hotmail.com>
2017-05-22 21:23:20 +0200

    Added methods: Fill, BubbleSort and Print

--- BubbleSort/BubbleSort/Program.cs
+++ BubbleSort/BubbleSort/Program.cs
@@ -10,6 +10,46 @@ namespace BubbleSort
     {
         static void Main(string[] args)
         {
+            int[] numbers = new int[10];
+            Fill(numbers);
+            BubbleSort(numbers);
+            Print(numbers);           
+        }
+
+        private static void Fill(int[] array)
+        {
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                array[i] = random.Next(1, 100);
+            }
+        }
+
+        private static void BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int i2 = 0; i2 < array.Length; i2++)
+                {
+                    if (!(i2 + 1 == array.Length)) { 
+                        int current = array[i2];
+                        int next = array[i2 + 1];
+                        if (current > next)
+                        {
+                            array[i2] = next;
+                            array[i2 + 1] = current;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void Print(int[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
